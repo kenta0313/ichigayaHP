c98ed27a248d00b0f3e7088811c34c69a1aa87aa
kenta0313 <asanokenta@me.com>
2021-06-18 05:15:34 +0900

    tailwindcss

--- src/components/organisms/Access.tsx
+++ src/components/organisms/Access.tsx
@@ -12,7 +12,7 @@ const Access = () => {
                     <p className="mt-6 text-green-700 mb-3">◇住所</p>
                     <p>〒102-0073<br/>東京都千代田区九段北<br/>４丁目２−１５</p>
                 </div>
-                <div className="flex-1 mt-9 md:mt-0 hidden md:inline-block">
+                <div className="flex-1 hidden md:inline-block">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d210.4762178520526!2d139.73695560520622!3d35.6916775304059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c60b5d2c805%3A0x2a12a7bb6c4f5488!2z44CSMTAyLTAwNzMg5p2x5Lqs6YO95Y2D5Luj55Sw5Yy65Lmd5q615YyX77yU5LiB55uu77yS4oiS77yR77yVIOW4guODtuiwt-ODl-ODqeOCtg!5e0!3m2!1sja!2sjp!4v1623920470861!5m2!1sja!2sjp" width="300" height="300" loading="lazy"></iframe>
                 </div>
             </div>
