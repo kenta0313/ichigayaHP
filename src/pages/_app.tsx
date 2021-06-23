6ed87c4f1a692bbf0f9d9df16d75c0d83739c1b7
kenta0313 <asanokenta@me.com>
2021-06-23 16:10:17 +0900

    app update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,9 @@ import { ThemeProvider } from "@material-ui/core/styles"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import theme from "../components/utils/theme"
 import "../styles/globals.css"
+import 'tailwindcss/tailwind.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-
 
   return (
     <React.Fragment>
