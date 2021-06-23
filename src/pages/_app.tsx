951547ce136dea63c44e8b0593acf11f7ba3bf0e
kenta0313 <asanokenta@me.com>
2021-06-23 16:04:36 +0900

    serversiderendring css true

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,13 +8,6 @@ import "../styles/globals.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side")
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles)
-    }
-  }, [])
 
   return (
     <React.Fragment>
