2b24f08e4ace99b1809b588df8367596b280971a
kenta0313 <asanokenta@me.com>
2021-06-23 13:53:57 +0900

    map change

--- src/components/organisms/Access.tsx
+++ src/components/organisms/Access.tsx
@@ -13,7 +13,7 @@ const Access = () => {
                     <p>〒102-0073<br/>東京都千代田区九段北<br/>４丁目２−１５</p>
                 </div>
                 <div className="flex-1 hidden md:inline-block">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d210.4762178520526!2d139.73695560520622!3d35.6916775304059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c60b5d2c805%3A0x2a12a7bb6c4f5488!2z44CSMTAyLTAwNzMg5p2x5Lqs6YO95Y2D5Luj55Sw5Yy65Lmd5q615YyX77yU5LiB55uu77yS4oiS77yR77yVIOW4guODtuiwt-ODl-ODqeOCtg!5e0!3m2!1sja!2sjp!4v1623920470861!5m2!1sja!2sjp" width="300" height="300" loading="lazy"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1558.9963505725927!2d139.73531408186247!3d35.69170566722533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d5d084f00e9%3A0x3d42c026d39d5482!2z5biC44O26LC344Kz44Ov44O844Kt44Oz44Kw44Kr44OV44Kn!5e0!3m2!1sja!2sjp!4v1624423500428!5m2!1sja!2sjp" width="300" height="300" loading="eager"></iframe>
                 </div>
             </div>
         </div>
