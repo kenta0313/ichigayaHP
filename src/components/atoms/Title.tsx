40cd4a336133d99fa4ad1d55a4ed0ff81a319355
kenta0313 <asanokenta@me.com>
2021-06-30 11:39:58 +0900

    mx change

--- src/components/atoms/Title.tsx
+++ src/components/atoms/Title.tsx
@@ -7,7 +7,7 @@ interface Titleprops {
 const Title = (props: Titleprops) => {
     return (
         <div>
-            <h1 className="text-lg sm:text-2xl  md:text-4xl text-center font-bold my-3 md:my-8"><span className="text-green-700">～</span>{props.title}<span className="text-green-700">～</span></h1>
+            <h1 className="text-lg sm:text-2xl  md:text-4xl text-center font-bold my-6 md:my-8"><span className="text-green-700">～</span>{props.title}<span className="text-green-700">～</span></h1>
         </div>
     )
 }
