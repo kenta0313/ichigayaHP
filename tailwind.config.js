4c7cd536561824547a3fafdd0c8f2e87d996e07c
kenta0313 <asanokenta@me.com>
2021-06-30 18:45:46 +0900

    package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +8,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  important: true,
 }
